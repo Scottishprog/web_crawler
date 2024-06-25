bf60981cdf6bc25101771810763ba74e8dd16a6a
Scottishprog <m_wanamaker_jr@sbcglobal.net>
2024-06-25 16:03:45 -0700

    Finally got URL extraction to text working, passed the first test.

--- crawl.js
+++ crawl.js
@@ -1,6 +1,5 @@
 import {URL} from 'url';
 import {JSDOM} from "jsdom";
-import jsDOM from "jsdom/lib/jsdom/living/attributes.js";
 
 function normalizeURL(inputURL) {
     const sourceURL = new URL(inputURL);
@@ -14,10 +13,10 @@ function stripTrailingSlash(input) {
     return input.endsWith('/') ? input.slice(0, -1) : input;
 }
 
-function getURLSFromHTML(htmlBody, baseURL) {
-    //const dom = new JSDOM(htmlBody);
-    //return dom.window.document.querySelectorAll("a")
-    return ""
+function getURLsFromHTML(htmlBody, baseURL) {
+    const dom = new JSDOM(htmlBody);
+    const result = dom.window.document.querySelectorAll("a")
+    return new Array(result)
 }
 
-export function getURLsFromHTML(htmlBody, baseURL) {}
+export { getURLsFromHTML }
--- crawl.test.js
+++ crawl.test.js
@@ -7,36 +7,40 @@ const test_string_2 = "https://blog.boot.dev/path"
 const test_string_3 = "http://blog.boot.dev/path/"
 const test_string_4 = "http://blog.boot.dev/path"
 
-test('Any Result at All', ()=> {
+test('Any Result at All', () => {
     expect(normalizeURL(test_string)).not.toBe("")
 })
 
-test('test_string normalized', ()=> {
+test('test_string normalized', () => {
     expect(normalizeURL(test_string)).toBe(test_string_r)
 })
 
-test('test_string_2 normalized', ()=> {
+test('test_string_2 normalized', () => {
     expect(normalizeURL(test_string_2)).toBe(test_string_r)
 })
 
-test('test_string_3 normalized', ()=> {
+test('test_string_3 normalized', () => {
     expect(normalizeURL(test_string_3)).toBe(test_string_r)
 })
 
-test('test_string_4 normalized', ()=> {
+test('test_string_4 normalized', () => {
     expect(normalizeURL(test_string_4)).toBe(test_string_r)
 })
 
 // Testing getURLsFromHTML
 const base_URL = 'https://blog.boot.dev';
 const html_1 =
-    '<html>\n' +
-    '    <body>\n' +
-    '        <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>\n' +
-    '    </body>\n' +
+    '<html>' +
+    '   <body>'+
+    '       <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>'+
+    '       <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>'+
+    '   </body>'+
     '</html>';
-const html_1_r = 'https://blog.boot.dev';
+const html_1_r = 'https://blog.boot.dev/';
 
 test('Basic URL from HTML', () => {
-    expect(getURLsFromHTML(html_1, base_URL)).toBe(html_1_r)
+    const result = getURLsFromHTML(html_1, base_URL);
+    console.log("Result: " + result[0].item(0).href);
+    const text = result[0].item(0).href
+    expect(text).toBe(html_1_r);
 })
