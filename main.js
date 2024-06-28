de7fccb3c6f56f3e086527bd0bf73078b78894d0
Scottishprog <m_wanamaker_jr@sbcglobal.net>
2024-06-28 15:14:34 -0700

    Added input argument handling in main.js.

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+import {argv} from 'node:process'
+
+function main(){
+    if(process.argv.length < 3){
+        console.error('Usage: npm run start BASE_URL - One argument is required.')
+    }else if (process.argv.length > 3){
+        console.error('Usage: npm run start BASE_URL - One argument is required.')
+    }else {
+        console.log(`Staring web crawler with URL: ${argv[2]}`)
+    }
+    console.log("Hello World")
+}
+
+
+main()
+
